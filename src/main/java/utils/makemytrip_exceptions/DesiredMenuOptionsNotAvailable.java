9823f3fb327264182878a29cf93c56c26980ed6d
evnraja32 <evnraja32@gmail.com>
2017-02-16 15:52:53 +0530

    Commit#2 from raja branch 8:41pm (#6)
    Signed-off-by: evnraja32 <evnraja32@gmail.com>

--- src/main/java/utils/makemytrip_exceptions/DesiredMenuOptionsNotAvailable.java
+++ src/main/java/utils/makemytrip_exceptions/DesiredMenuOptionsNotAvailable.java
@@ -6,7 +6,11 @@ public class DesiredMenuOptionsNotAvailable extends Exception {
 	public DesiredMenuOptionsNotAvailable() {
 		/*This class will be called when certain Options we expected are not visible in the page 
 		 */
+//<<<<<<< raja
+	//	System.err.println("The option you are refering to is not available in this page...");
+//=======
 		System.err.println("The option you are refering to is not available in this page -a change done by asif");
+//>>>>>>> asif
 	}
 
 }
